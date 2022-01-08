98810eddfcc2d94cac7c55f9a6986a240f54f270
Terlick <meozkara@gmail.com>
2022-01-08 13:34:13 +0000

    Add support for favicon

--- clipperbot/webserver/serveclips.py
+++ clipperbot/webserver/serveclips.py
@@ -56,6 +56,8 @@ def get_link(clip_fname:str):
 
 
 def favicon_response(request):
+    if os.path.isfile("favicon.ico"):
+        return FileResponse("favicon.ico")
     if os.path.isfile("favicon.png"):
         return FileResponse("favicon.png")
     else:
