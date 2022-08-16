7fa67715e7d367d44c55585e525fab081735e17b
OdielDomanie <>
2022-08-16 19:22:20 +0000

    Change configs

--- config.py
+++ config.py
@@ -7,18 +7,18 @@ MAX_DURATION = 300
 DEFAULT_PREFIX = "__"
 
 DOWNLOAD_DIR = "downloads"
-MAX_DL_SIZE = 10**9
+MAX_DL_SIZE = 10 * 10**9
 CLIP_DIR = "clips"
-MAX_CLIPS_SIZE = 10**9
+MAX_CLIPS_SIZE = 2 * 10**9
 DATABASE = "clipperbot.db"
 CHANNELS_LIST_DB = "channels.db"
 STREAMS_DB = "streams.db"
 
-LOG_LVL = logging.DEBUG
+LOG_LVL = logging.INFO
 UVICORN_LOG_LVL = logging.INFO
 
 FFMPEG = "ffmpeg"
 
 POLL_PERIOD = 60
 
-URL_PORT = 8082
+URL_PORT = 80
